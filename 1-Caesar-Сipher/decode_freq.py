5383d81bc9417d76e85b1f625dbe401f2ccf5dcc
Andrey Reynov <79510793+andreyreynov@users.noreply.github.com>
2024-05-14 18:13:28 +0300

    Moved branches

---
+++ 1-Caesar-Сipher/decode_freq.py
@@ -0,0 +1,8 @@
+from collections import Counter
+import frequencies
+
+message = "Amr11Hifpsex mw e wmqtpi erh pmklxaimklx tsaivwlipp wgvmtx xlex viqsziw tvi-mrwxeppih Amrhsaw fpsexaevi ettw, hmwefpiw xipiqixvc erh higpyxxivw xli ibtivmirgi fc hmwefpmrk sv viqszmrk mrxvywmzi mrxivjegi ipiqirxw, ehw erh qsvi. Rs riih xs temrwxeomrkpc ks xlvsykl epp xli wixxmrkw csyvwipj, sv viqszi ettw sri fc sri. Amr11Hifpsex qeoiw xli tvsgiww uymgo erh iewc! Csy ger tmgo erh glsswi ibegxpc almgl qshmjmgexmsrw csy aerx xli wgvmtx xs qeoi, sv ywi xli hijeypx wixxmrkw. Mj csy evi yrlettc amxl erc sj xli glerkiw csy ger iewmpc vizivx xliq fc ywmrk xli vikmwxvc jmpiw xlex evi mrgpyhih mr xli 'Vikjmpiw' jsphiv, epp sj xli ettw xlex evi viqszih ger fi vimrwxeppih jvsq xli Qmgvswsjx wxsvi."
+
+message = message.upper()
+
+print(frequencies.GetFrequencies().get_digram_percentage(message, 2))
